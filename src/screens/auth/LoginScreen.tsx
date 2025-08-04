ffaa86c70455b620802159bb3240b0b2fe9db5db
131c2 <bg3189@naver.com>
2025-08-04 15:56:05 +0900

    feat: 기본 입력란 추가

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,49 @@
-import { Text } from '@/components/common';
-import { View } from 'react-native';
+import { Button, Input, Text } from '@components/common';
+import styled from '@emotion/native';
+import React, { useState } from 'react';
 
-export default function LoginScreen() {
+const ContentWrapper = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const Title = styled(Text)`
+  font-size: 32px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>LoginScreen!</Text>
-    </View>
+    <ContentWrapper>
+      <Title>smooth</Title>
+
+      <Input
+        label='이메일'
+        value={email}
+        onChangeText={setEmail}
+        placeholder='이메일을 입력해주세요'
+        keyboardType='email-address'
+      />
+
+      <Input
+        label='비밀번호'
+        value={password}
+        onChangeText={setPassword}
+        placeholder='비밀번호를 입력해주세요'
+        secureTextEntry
+      />
+
+      <Button label='로그인' onPress={() => console.log('로그인 클릭!')} />
+
+      <Text style={{ textAlign: 'center', marginTop: 20 }}>이메일로 가입하기</Text>
+    </ContentWrapper>
   );
-}
+};
+
+export default LoginScreen;
