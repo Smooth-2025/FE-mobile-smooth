920cdeb5d4a2ebad7d120a30be72c113351bae79
131c2 <bg3189@naver.com>
2025-08-04 18:33:06 +0900

    feat: SignupScreen navigation 추가

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import LoginScreen from '../screens/auth/LoginScreen';
+import SignupScreen from '../screens/auth/SignupScreen';
 import MainTabs from './MainTabs';
 
 const Stack = createNativeStackNavigator();
@@ -12,6 +13,7 @@ const AppNavigator = () => {
       <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
         {/* <Stack.Screen name='Splash' component={SplashScreen} /> */}
         <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Signup' component={SignupScreen} />
         <Stack.Screen name='Main' component={MainTabs} />
       </Stack.Navigator>
     </NavigationContainer>
