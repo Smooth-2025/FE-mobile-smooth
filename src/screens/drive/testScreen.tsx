19f2241982225e7e9f137be0a38a4eb1e0009055
suin <73993670+whl5105@users.noreply.github.com>
2025-08-04 12:00:07 +0900

    feat: 공통 폴더, 컴포넌트 구성 및 레이아웃 적용 #1

---
+++ src/screens/drive/testScreen.tsx
@@ -0,0 +1,5 @@
+import { Text } from '../../components/common';
+
+export default function TestScreen() {
+  return <Text>TestScreen</Text>;
+}
---
+++ src/screens/myPage/MypageScreen.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import { Text } from '../../components/common';
+
+export default function MypageScreen() {
+  return (
+    <View>
+      <Text>MypageScreen!</Text>
+    </View>
+  );
+}
