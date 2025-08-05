3c6e8a1d2750d9fc46672c2cd4f60dfbc045dbe1
suin <73993670+whl5105@users.noreply.github.com>
2025-08-05 10:39:26 +0900

    feat: 주행상태에따른 상태관리 코드 추가 및 스타일 파일분리 #11
    - 이후 소켓담당 연결 후 추가 예정

--- src/components/camera/AutoCamera.tsx
+++ src/components/camera/AutoCamera.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Platform } from 'react-native';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import * as Styled from './AutoCamera.styles';
 
 export function AutoCamera() {
   const camera = useRef<Camera>(null);
@@ -13,6 +14,16 @@ export function AutoCamera() {
   const [isDriving, setIsDriving] = useState(true);
   //카메라 준비
   const [isCameraReady, setIsCameraReady] = useState(false);
+
+  useEffect(() => {
+    //TODO: 소켓에 주행 상태에 따른 isDriving 조건 변경 로직
+    const timer = setTimeout(() => {
+      setIsDriving(false);
+      console.log('5초 후 주행 종료 → 카메라 비활성화');
+    }, 2000); // 5초 = 5000ms
+
+    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+  }, []);
 
   const onCameraInitialized = () => {
     console.log(`[${Platform.OS}] 카메라 초기화 완료`);
@@ -37,10 +48,10 @@ export function AutoCamera() {
   }
 
   return (
-    <View style={styles.overlay}>
+    <Styled.Overlay>
       <Camera
         ref={camera}
-        style={styles.camera}
+        style={{ flex: 1 }}
         device={device}
         isActive={isDriving}
         photo={true}
@@ -48,21 +59,6 @@ export function AutoCamera() {
         onInitialized={onCameraInitialized}
         onError={onCameraError}
       />
-    </View>
+    </Styled.Overlay>
   );
 }
-
-const styles = StyleSheet.create({
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'black',
-    zIndex: 1000,
-  },
-  camera: {
-    flex: 1,
-  },
-});
